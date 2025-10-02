d1050645c5e8f37910c9f841113243f92f83d1ff
TKNORD <122537123+TKNORD@users.noreply.github.com>
2025-10-02 11:27:25 +0200

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { DailyProvider } from "@daily-co/daily-react";
 
-import "./fonts/Christmas and Santona.ttf";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
